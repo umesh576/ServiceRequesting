7e42bf6f51451d77e43e8b555d55f49ef6581c51
Umesh Joshi <uj19343@gmail.com>
2025-11-24 13:18:24 +0545

    new update

--- src/main/java/org/zeroscope/demo/Controller/ServicesController.java
+++ src/main/java/org/zeroscope/demo/Controller/ServicesController.java
@@ -1,13 +1,13 @@
 package org.zeroscope.demo.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/services")
 public class ServicesController {
 
-  @RequestMapping("/services")
   @GetMapping("/add")
   public String addServices() {
-    return "umehs joshi is services";
+    return "Umesh Joshi is services";
   }
 }
--- src/main/java/org/zeroscope/demo/repository/ServicesRepository.java
+++ src/main/java/org/zeroscope/demo/repository/ServicesRepository.java
@@ -1,3 +1,6 @@
 package org.zeroscope.demo.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ServicesRepository {}
